27f085c5ba3a1a6d231751b137f88b3cb937a72e
tongla21 <wongkomgla@hotmail.com>
2016-09-06 20:25:43 +0700

    worawat wongkom 580611053 cpe200-week3-hw

--- src/cpe200/Course.java
+++ src/cpe200/Course.java
@@ -6,19 +6,27 @@ import java.util.regex.Pattern;
 public class Course {
 
     public Course() {
-        this("","","",30);
+        this("TBA","000000","TBA",30);
     }
 
     public Course(String n, String cid) {
         // implement here
+        this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
+        this.course_id = isValidCourse_id(cid)?cid:"000000";
     }
 
     public Course(String n, String cid, String l) {
         // implement here
+        this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
+        this.course_id = isValidCourse_id(cid)?cid:"000000";
+        this.lecturer = !l.equalsIgnoreCase("")?l:"TBA";
     }
 
     public Course(String n, String cid, String l, int max) {
         this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
+        this.course_id = isValidCourse_id(cid)?cid:"000000";
+        this.lecturer = !l.equalsIgnoreCase("")?l:"TBA";
+        this.max_students = (max>=10)?max:30;
         // implement the rest here
     }
 
@@ -41,29 +49,32 @@ public class Course {
     // implement the other get and set methods here
     public String getLecturer() {
         // implement here
-        return "lecturer";
+        return this.lecturer;
     }
 
     public void setLecturer(String lecturer) {
         // implement here
+        this.lecturer = !lecturer.equalsIgnoreCase("")?lecturer:this.lecturer;
     }
 
     public int getMax_students() {
         // implement here
-        return 0;
+        return this.max_students;
     }
 
     public void setMax_students(int max_students) {
         // implement here
+        this.max_students = (max_students>=10)?max_students:this.max_students;
     }
 
     public int getNo_students() {
         // implement here
-        return 0;
+        return no_students;
     }
 
     public void setNo_students(int no_students) {
         // implement here
+       this.no_students = (no_students>=0&&no_students<=getMax_students())?no_students:this.no_students;
     }
 
     @Override
@@ -73,7 +84,14 @@ public class Course {
                 + this.lecturer + ", has ";
 
         // implement the rest here
-
+        if(this.no_students == 0) {
+            o = o + "NO student, [maximum: " + max_students + "]";
+        }
+        else if(this.no_students == 1){
+            o = o+"ONE student, [maximum: "+max_students+"]";
+        }else{
+            o = o+this.no_students+" students, [maximum: "+max_students+"]";
+        }
         return o;
     }
 
@@ -82,8 +100,11 @@ public class Course {
         Matcher m = p.matcher(id);
 
         // implement the rest here
-
-        return true;
+        if(m.matches()){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     // Regular expression for the Student ID pattern
--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -1,30 +1,93 @@
 package cpe200;
 
 public class Student {
-
-    public Student() {
-        // implement here
+    public Student (){
+        this ("John Doe","560610000",1990,false);
     }
-
+    public Student(String s, String s1, int i, boolean b) {
+        this.name = !s.equalsIgnoreCase("")?s:"John Doe";
+        this.id = isValidStudent_id(s1)?s1:"560610000";
+        this.yob = isValidYOB(i)?i:1990;
+        this.isactivated = b;
+    }
     // implement all missing constructors here
-
+    public Student(String s, String s1, int i) {
+        this.name = !s.equalsIgnoreCase("")?s:"John Doe";
+        this.id = isValidStudent_id(s1)?s1:"560610000";
+        this.yob = isValidYOB(i)?i:1990;
+    }
     // implement all get and set methods here
+    public Student(String s, String s1) {
+        this.name = !s.equalsIgnoreCase("")?s:"John Doe";
+        this.id = isValidStudent_id(s1)?s1:"560610000";
+    }
 
     @Override
     public String toString() {
-        String o = "Something";
+        String o = this.name + " ("
+                + this.id + ") was born in "
+                + this.yob + " is an ";
+        if(this.isactivated == true){
+            o = o+"ACTIVE student.";
+        }else{
+            o = o+"INACTIVE student.";
+        }
+
 
         return o;
     }
 
     private boolean isValidStudent_id(String id) {
-        return true;
+        String pattern = "5[6-9]{1}061[0-2]{1}[0-9]{3}";
+        return id.matches(pattern);
     }
 
     private boolean isValidYOB(int yob) {
-        return true;
+        return (yob >= 1990)?true:false;
     }
 
     // declare your attributes here
+    public String getName() {
+        return name;
+    }
+    public void setName(String n){
+        this.name = !n.equalsIgnoreCase("")?n:this.name;
+    }
 
+    public String getStudent_id() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = isValidStudent_id(id)?id:this.id;
+    }
+
+    public int getYearOfBirth() {
+        return yob;
+    }
+
+    public void setYob(int yob) {
+        this.yob = isValidYOB(yob)?yob:this.yob;
+    }
+
+    public boolean isActive() {
+        return isactivated;
+    }
+
+    public void setActive(boolean active) {
+        isactivated = active;
+    }
+
+    private String name;
+    private String id;
+    private int yob;
+    private boolean isactivated;
+
+    public void setStudent_id(String student_id) {
+    this.id = isValidStudent_id(student_id)?student_id:this.id;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+    this.yob = isValidYOB(yearOfBirth)?yearOfBirth:this.yob;
+    }
 }
